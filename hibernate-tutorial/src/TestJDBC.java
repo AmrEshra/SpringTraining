c752fbec38e72c55f8dafacc86c104511a1ecffa
Amr Eshra <amr.eshra@gmail.com>
2019-12-29 11:23:31 +0300

    get

--- hibernate-tutorial/src/TestJDBC.java
+++ hibernate-tutorial/src/TestJDBC.java
@@ -1,30 +1,42 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.sql.Insert;
 
 import orm.WorkingDay;
 
 public class TestJDBC {
 
+	static SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(WorkingDay.class).buildSessionFactory();
+	static Session session = factory.getCurrentSession();
+	
 	public static void main(String[] args) {
-
-		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(WorkingDay.class).buildSessionFactory();
-		Session session = factory.getCurrentSession();
-
 		try {
-			WorkingDay day = new WorkingDay("Amr", "26-12-2019", "14:00");
 			session.beginTransaction();
-			session.save(day);
+//			insert();
+			get(122L);
 			
-			session.getTransaction().commit();
 
 		} catch (Exception exc) {
 			exc.printStackTrace();
 			session.getTransaction().rollback();
 		} finally {
 			factory.close();
+			System.out.println("Done");
 		}
 
+	}
+	
+	private static void get(Long i) {
+		WorkingDay d = session.get(WorkingDay.class, i);
+		System.out.println(d);
+	}
+
+	public static void insert() {
+		
+		WorkingDay day = new WorkingDay("Amr", "26-12-2019", "14:00");
+		session.save(day);
+		session.getTransaction().commit();
 	}
 
 }
--- hibernate-tutorial/src/orm/WorkingDay.java
+++ hibernate-tutorial/src/orm/WorkingDay.java
@@ -2,21 +2,28 @@ package orm;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name="Amr")
 public class WorkingDay {
 
 	@Id
+	@SequenceGenerator(name = "PRL_ATTACHMENTS_SEQ", sequenceName = "PRL_ATTACHMENTS_SEQ", allocationSize = 1)  
+	@GeneratedValue(generator = "PRL_ATTACHMENTS_SEQ", strategy = GenerationType.SEQUENCE)
 	@Column(name="ID")
-	private long id;
+	private Long id;
 	
 	@Column(name="NAME")
 	private String name;
@@ -31,6 +38,11 @@ public class WorkingDay {
 		this.name = name;
 		this.date = date;
 		this.time = time;
+	}
+
+	@Override
+	public String toString() {
+		return "WorkingDay [id=" + id + ", name=" + name + ", date=" + date + ", time=" + time + "]";
 	}
 	
 }
