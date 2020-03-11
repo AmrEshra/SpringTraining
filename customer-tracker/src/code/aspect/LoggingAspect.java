303c837f8a5e8185553de92ac4dcce4d62c241fb
Amr Eshra <amr.eshra@gmail.com>
2020-03-11 09:39:46 +0300

    .

--- customer-tracker/src/code/aspect/LoggingAspect.java
+++ customer-tracker/src/code/aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package code.aspect;
 
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
 	@Before("execution(* code.services.CustomerServiceImpl.getCustomers())")
+//	@Before("execution(* getCustomers())") any getCustomers at any class
+//	@Before("execution(* get * ())") any get at any class
 	public void beforeGetCustomersAdvice() {
 		System.out.println("\n=====>>> Executing @Before advice on getCustomers()");
 	}
@@ -18,6 +21,13 @@ public class LoggingAspect {
 	public void afterGetCustomersAdvice() {
 		System.out.println("\n=====>>> Executing @@After advice on getCustomers()");
 	}
+	
+	/*
+	 * @Around ("execution(* code.services.CustomerServiceImpl.getCustomers())")
+	 * public void aroundGetCustomersAdvice() {
+	 * System.out.println("\n=====>>> Executing @@After advice on getCustomers()");
+	 * }
+	 */
 }
 
 
