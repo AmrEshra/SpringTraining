b0689e0f6f532b3493b83d252ebf5188be45b204
Amr Eshra <amr.eshra@gmail.com>
2020-04-27 16:45:51 +0300

    customerTrackerSpringboot

---
+++ customerTrackerSpringboot/src/main/java/com/customerTrackerSpringboot/CustomerTrackerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.customerTrackerSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerTrackerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTrackerSpringbootApplication.class, args);
+	}
+
+}
---
+++ customerTrackerSpringboot/src/test/java/com/customerTrackerSpringboot/CustomerTrackerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customerTrackerSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerTrackerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
